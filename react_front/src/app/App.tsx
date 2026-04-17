a7226d10b46a26d081ca4919aacaa8815983621d
F <brother.23@yandex.ru>
2026-04-17 23:00:46 +0300

    рабочий фронт

--- react_front/src/app/App.tsx
+++ react_front/src/app/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import "../styles/index.css";  // ← ЭТО ВСЁ! Tailwind + Figma дизайн
 
 export default function App() {
   return <RouterProvider router={router} />;
